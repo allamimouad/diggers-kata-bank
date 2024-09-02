bfcb3d08487b08a0c096fa00c9004b9a05754d09
ALLAMI Mouad <allamimouad@gmail.com>
2024-09-02 04:12:07 +0200

    init

---
+++ src/main/java/diggers/kata/App.java
@@ -0,0 +1,16 @@
+package diggers.kata;
+
+import java.util.Scanner;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+
+
+}
