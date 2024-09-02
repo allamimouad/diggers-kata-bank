30f8f36da37a89fd75c864482a0e888cbef7c121
ALLAMI Mouad <allamimouad@gmail.com>
2024-09-02 06:28:24 +0200

    TransactionHistoryPrinter#print implementation

--- src/main/java/diggers/kata/TransactionHistoryPrinter.java
+++ src/main/java/diggers/kata/TransactionHistoryPrinter.java
@@ -5,6 +5,11 @@ import java.util.List;
 public class TransactionHistoryPrinter {
 
     public static void print(List<Transaction> transactions) {
-
+        System.out.println("Date\t\t\tAmount\t\tBalance");
+        for (Transaction transaction : transactions) {
+            System.out.println(transaction.getDate() + "\t" +
+                    transaction.getAmount() + "\t\t" +
+                    transaction.getBalanceAfterTransaction());
+        }
     }
 }
